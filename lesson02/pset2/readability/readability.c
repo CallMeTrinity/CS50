d3f7fe5b85ea0e5da7ae780d471534f606c1c79d
CallMeTrinity <anteur.ap@gmail.com>
2025-04-07 14:11:13 +0200

    pset2 : working readability.c

--- lesson02/pset2/readability/readability.c
+++ lesson02/pset2/readability/readability.c
@@ -1,51 +1,86 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-int get_avg_letters(string text);
+float get_avg_letters(string text);
 int get_letter_count(string word);
+int get_word_count(string text);
+int get_sentence_count(string t);
+int get_index(float l, int s, int w);
+
 int main(void)
 {
     string text = get_string("Text : ");
-    printf("%i\n", get_letter_count(text));
-    printf("%i\n", get_avg_letters(text));
-    // return the avg number of letters per words in the text
+    int lcount = get_letter_count(text);
+    int wcount = get_word_count(text);
+    int scount = get_sentence_count(text);
+    int index = get_index(lcount, scount, wcount);
+
+    if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
 
-// index = 0.0588 * L - 0.296 * S - 15.8
-// L = avg nb of letter, S = average number of sentence per 100 words
-
-int get_avg_letters(string text)
+int get_index(float l, int s, int w)
 {
-    string words[255];
-
-    for (int i = 0, l = strlen(text); i < l; i++)
-    {
-        if (text[i] != ' ')
-        {
-            printf("text[%i] : %c\n", i, text[i]);
-            words[i] += text[i];
-            printf("words[%i] : %s\n", i, words[i]);
-        }
-    }
-    int sum = 0;
-    for (int i = 0, l = sizeof(words); i < l; i++)
-    {
-        sum += strlen(words[i]);
-    }
-
-    return sum / sizeof(words);
+    l = (100 * l) / w;
+    s = (100 * s) / w;
+    return (0.0588 * l) - (0.296 * s) - 15.8;
 }
 
-// compte le nombre de lettre dans un string sans les espaces, a voir mais si je peux séparer les mots tout est faisable avec strlen.
-int get_letter_count(string word)
+float get_avg_letters(string text)
+{
+    int lcount = get_letter_count(text);
+    int wcount = get_word_count(text);
+    int scount = get_sentence_count(text);
+    int index = get_index(lcount, scount, wcount);
+
+    return (float)lcount / wcount;
+}
+
+int get_letter_count(string s)
 {
     int counter = 0;
-    for (int i = 0, l = strlen(word); i < l; i++)
+    for (int i = 0, l = strlen(s); i < l; i++)
     {
-        if (word[i] != 32)
+        if (s[i] != ' ')
         {
             counter++;
         }
     }
     return counter;
+}
+
+int get_word_count(string text)
+{
+    int counter = 1; // on compte les espaces pour déterminer le nombre de mots donc on rajoute 1 pour le premier mot.
+    for (int i = 0, l = strlen(text); i < l; i++)
+    {
+        if (text[i] == ' ')
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int get_sentence_count(string t)
+{
+    int c = 0;
+    for (int i = 0, l = strlen(t); i < l; i++)
+    {
+        if (t[i] == '.' || t[i] == '?' || t[i] == '!')
+        {
+            c++;
+        }
+    }
+    return c;
 }
